a553c419f02135090ed2915f7a43d5d538ec6944
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-03 21:53:53 -0500

    more changes using bootstrap

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,7 +14,7 @@ requirejs(["dom-access", "populate-songs", "get-more-songs"],
 
 function addSongs(data){
   for(var i=1; i<=data.songs.length; i++){
-    $('#more').before("<div class='container'><p class='large' id='song'>"+data.songs[i-1].name+"</p><ul><li id='artist'>"+data.songs[i-1].artist+"</li><li class='middle' id='album'>"+data.songs[i-1].album+"</li><li id='year'>"+data.songs[i-1].year+"</li></ul><button type='button' class='btn btn-default btn-sm delete'>Delete</button></div>");
+    $('#more').before("<div class='container'><p class='large' id='song'>"+data.songs[i-1].name+"</p><ul><li id='artist'> Artist: "+data.songs[i-1].artist+"</li><li class='middle' id='album'>Album: "+data.songs[i-1].album+"</li><li id='year'>Year: "+data.songs[i-1].year+"</li></ul><button type='button' class='btn btn-default btn-sm delete'>Delete</button></div>");
   }
   initDelete();
 }
