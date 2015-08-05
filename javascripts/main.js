66f37a1c23402c9028b7ac9b0e47de58c17e263b
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-05 09:34:41 -0500

    removed media queries, added matchHeight

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
 requirejs(["dom-access", "populate-songs", "get-more-songs"],
- function(output, generate, getMore) {
-  var num = 1;
-  console.log(output);
-  generate.setArray(addSongs);
+  function(output, generate, getMore) {
+    var num = 1;
+    console.log(output);
+    generate.setArray(addSongs);
 
   $('#more').click(function(){
     if(num===1){
@@ -16,6 +16,7 @@ function addSongs(data){
   for(var i=1; i<=data.songs.length; i++){
     $('#more').before("<div class='container'><h5 class='large' id='song'>"+data.songs[i-1].name+"</h5><ul><li id='artist'> Artist: "+data.songs[i-1].artist+"</li><li class='middle' id='album'>Album: "+data.songs[i-1].album+"</li><li id='year'>Year: "+data.songs[i-1].year+"</li></ul><button type='button' class='btn btn-default btn-sm delete'>Delete</button></div>");
   }
+      $('.matchHeight').matchHeight();
   initDelete();
 }
 
